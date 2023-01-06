c8840c33d4c3981bcbcda8505ac9107c7ba384f6
ajur-ajur <lazuardialdira.form@gmail.com>
2023-01-07 06:52:55 +0700

    Run Once

---
+++ src/main/RUN THIS ONCE/runonlyonce.java
@@ -0,0 +1,25 @@
+package com.database;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+public class runonlyonce {
+   public static void main(String args[]) throws Exception {
+      //Registering the Driver
+      DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+      //Getting the connection
+      String mysqlUrl = "jdbc:mysql://localhost/orayorayan";
+      Connection con = DriverManager.getConnection(mysqlUrl, "root", "");
+      System.out.println("Connection established......");
+      //Initialize the script runner
+      ScriptRunner sr = new ScriptRunner(con);
+      //Creating a reader object
+      Reader reader = new BufferedReader(new FileReader("RUN THIS ONCE/runonlyonce.sql"));
+      //Running the script
+      sr.runScript(reader);
+   }
+}
