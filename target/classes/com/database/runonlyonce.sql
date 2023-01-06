e7deab8d03c3806895b3d4bbc079623c8f680246
ajur-ajur <lazuardialdira.form@gmail.com>
2023-01-07 01:39:16 +0700

    1.1

---
+++ target/classes/com/database/runonlyonce.sql
@@ -0,0 +1,70 @@
+CREATE DATABASE orayorayan;
+use orayorayan;
+
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 06, 2023 at 04:52 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `orayorayan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orayorayan`
+--
+
+CREATE TABLE `orayorayan` (
+  `id_player` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `score` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orayorayan`
+--
+
+INSERT INTO `orayorayan` (`id_player`, `user_name`, `score`) VALUES
+(1, 'DEWA_ULAR', 100),
+(2, 'DEWI_ULAR', 50);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orayorayan`
+--
+ALTER TABLE `orayorayan`
+  ADD PRIMARY KEY (`id_player`,`score`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orayorayan`
+--
+ALTER TABLE `orayorayan`
+  MODIFY `id_player` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
